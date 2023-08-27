9e0fbdd86043adf78ad01110b50e0b0c68b41bf2
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-08-27 17:14:52 +0530

    added new items

--- style.js
+++ style.js
@@ -1,8 +1,31 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor = "green";
+//var items = document.getElementsByTagNameNS('list-group-item');
+//console.log(items);
+//console.log(items[2]);
+//items[2].style.backgroundColor = "green";
 
-for(var i = 0;i < items.length;i++){
-    items[i].style.fontWeight = 'bold';
-}
+//for(var i = 0;i < items.length;i++){
+    //items[i].style.fontWeight = 'bold';
+//}
+
+//by getElementByClassname
+var listItems = document.getElementsByClassName('list-group-item');
+
+var newLi = document.createElement('li');
+newLi.textContent = 'New Item';
+
+
+listItems[0].parentNode.appendChild(newLi);
+
+//by getElementById
+var list = document.getElementById('items');
+var listItems = list.getElementsByTagName('li');
+
+
+var newLi = document.createElement('li');
+newLi.textContent = 'New Item';
+
+
+list.appendChild(newLi);
+
+
+

a259b6657ec542e43b3a638c21f1d356b1b21080
lakshmiichoudhary <laxmi2252001@gmail.com>
2023-08-27 17:52:27 +0530

    changed 2nd element and odd element color

--- style.js
+++ style.js
@@ -8,24 +8,31 @@
 //}
 
 //by getElementByClassname
-var listItems = document.getElementsByClassName('list-group-item');
+//var listItems = document.getElementsByClassName('list-group-item');
 
-var newLi = document.createElement('li');
-newLi.textContent = 'New Item';
+//var newLi = document.createElement('li');
+//newLi.textContent = 'New Item';
 
 
-listItems[0].parentNode.appendChild(newLi);
+//listItems[0].parentNode.appendChild(newLi);
 
 //by getElementById
-var list = document.getElementById('items');
-var listItems = list.getElementsByTagName('li');
+//var list = document.getElementById('items');
+//var listItems = list.getElementsByTagName('li');
 
 
-var newLi = document.createElement('li');
-newLi.textContent = 'New Item';
+//var newLi = document.createElement('li');
+//newLi.textContent = 'New Item';
 
 
-list.appendChild(newLi);
+//list.appendChild(newLi);
 
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = "blue";
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i =0; i < odd.length; i++){
+    odd[i].style.backgroundColor = "green";
+}
 
 
